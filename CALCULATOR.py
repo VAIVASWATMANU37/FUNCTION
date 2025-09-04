7e88e3ba27f2e076d558eee36cf8ed631a4b2991
VAIVASWATMANU37 <vaivaswatmanu13@gmail.com>
2025-09-04 20:50:24 +0530

    IDK

---
+++ CALCULATOR.py
@@ -0,0 +1,10 @@
+SRT=input("CHOOSE WHAT DO U WANT TO CALCULATE:-"
+          "1.ADDITION",
+          "2.SUBTRACTION",
+          "3.MULTIPLICATION",
+          "4.DIVISION")
+A1=int(input("ENTER THE FIRST NO."))
+A2=int(input("ENTER THE SECOND NO."))
+def ADD():
+    print(A1+A2)
+ADD()
