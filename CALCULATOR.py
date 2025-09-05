7d1f7ae1b82a4a284d0431fa9af295526d6cb2d7
VAIVASWATMANU37 <vaivaswatmanu13@gmail.com>
2025-09-05 20:44:10 +0530

    idk

--- CALCULATOR.py
+++ CALCULATOR.py
@@ -1,10 +1,27 @@
-SRT=input("CHOOSE WHAT DO U WANT TO CALCULATE:-"
-          "1.ADDITION",
-          "2.SUBTRACTION",
-          "3.MULTIPLICATION",
-          "4.DIVISION")
-A1=int(input("ENTER THE FIRST NO."))
-A2=int(input("ENTER THE SECOND NO."))
+print("CHOOSE WHAT YOU WANT TO CALCULATE:")
+print("1. ADDITION")
+print("2. SUBTRACTION")
+print("3. MULTIPLICATION")
+print("4. DIVISION")
+SRT = int(input("ENTER YOUR CHOICE"))
+A1 = int(input("ENTER THE FIRST NO."))
+A2 = int(input("ENTER THE SECOND NO."))
 def ADD():
-    print(A1+A2)
-ADD()
+    print(A1 + A2)
+def SUB():
+    print(A1 - A2)
+def MUL():
+    print(A1 * A2)
+def DIV():
+    print(A1 / A2)
+if SRT == 1:
+    ADD()
+elif SRT == 2:
+    SUB()
+elif SRT == 3:
+    MUL()
+elif SRT == 4:
+    DIV()
+else:
+    print("ERROR 69420!!!" 
+          "INVALID CHOICE!!!")
---
+++ CIRCUMFERENCE.py
@@ -0,0 +1,5 @@
+SRT=int(input("ENTER THE RADIUS!"))
+def SRT10():
+    VIPER= 2*3.1416*SRT
+    print(VIPER)
+SRT10()
